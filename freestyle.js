b0d14afaa7c7624172ff45902fe63482971fe95c
OliviaJorasch <olijora@nuevaschool.org>
2017-08-11 16:56:40 -0700

    added buttons to change word times

---
+++ freestyle.js
@@ -0,0 +1,98 @@
+const sounds = [
+    '1. no games 165 prod by Chase Moore',
+    'advancement',
+    'bob prod by chase moore 167 1',
+    'CAME TO ROCK',
+    'vienna173 prod. by chase moore',
+];
+const countdownTimer = 5000;
+var wordTimer = 7000;
+var current_level=1;
+var word_levels = {1:[],2:[],3:[]};
+var index1 = 0;
+var index2 = 0;
+
+let audioPlayer = document.getElementById('player');
+let timer = new Timer(countdownTimer, document.getElementById('countdown'), [], wordTimer, current_level);
+
+let startFreestyle = (topic) => {
+    $('#menuWrapper').animate({
+        top: '100%'
+    }, 400, () => {
+        $('.actions').show();
+        $('.countdown-container').css('display', 'flex');
+
+        $.get(`data/${topic}.csv`)
+            .done((csv) => {
+                let words = [];
+                try {
+                    words = $.csv.toArrays(csv, { separator: ',', delimiter: '"', startIndex: 2});
+                } catch (error) {
+                    console.error(error);
+                    // TODO display error
+                    stopFreestyle();
+                }
+                // split words into dictionary with different levels
+                for (let i=1; i<words.length; i++) {
+                  let level=parseInt(words[i][5]);
+                  let word=words[i];
+                  word_levels[level].push(word);
+                }
+                // randomly switch the order of words in the dictionary
+                for (let i=1; i<4; i++) {
+                  for (let j=0; j<word_levels[i].length; j++) {
+                    var randomInt=Math.floor(Math.random() * word_levels[i].length);
+                    index1=word_levels[i][j];
+                    index2=word_levels[i][randomInt];
+                    word_levels[i][j]=index2;
+                    word_levels[i][randomInt]=index1;
+                  }
+                  for (let k=0; k<word_levels[i].length; k++) {
+                  }
+                }
+                timer.setWords(word_levels[current_level]);
+                timer.start();
+            })
+            .fail(() => {
+                console.error(`Error while loading the CSV`);
+                // TODO display error
+                stopFreestyle();
+            });
+    });
+};
+
+let changeTime = (change) => {
+  if (wordTimer>1000 || change>0) {
+    wordTimer+=change*1000;
+    timer.setTime(wordTimer);
+  }
+}
+let stopFreestyle = () => {
+    audioPlayer.pause();
+    timer.reset();
+
+    $('.countdown-container').hide();
+    $('.actions').hide();
+    $('#menuWrapper').animate({
+        top: 0
+    }, 400);
+};
+let newList = (level) => {
+  current_level = level;
+  timer.setWords(word_levels[level]);
+}
+$('#countdown').click((e) => {
+    timer.toggleDefinition();
+});
+$(document).keypress((e) => {
+  timer.checkKey(e);
+});
+$(".topic").click((e) => {
+    let topicName = $(e.currentTarget).data('name')
+
+    if (topicName) {
+        audioPlayer.src = `sounds/${sounds[Math.floor(Math.random() * sounds.length)]}.mp3`;
+        audioPlayer.play(); // Play the empty element to get control
+        startFreestyle(topicName);
+    }
+});
---
+++ timer.js
@@ -0,0 +1,137 @@
+class Timer {
+    constructor(duration, element, words, wordTimer, current_level) {
+        let self = this;
+        this.duration = duration;
+        this.element = element;
+        this.words = words;
+        this.wordTimer = wordTimer;
+        this.running = false;
+        this.current_level = current_level;
+        this.index = 0;
+        this.els = {
+            ticker: document.getElementById('ticker'),
+            seconds: document.getElementById('seconds'),
+            definition: document.getElementById('definition'),
+        };
+    }
+
+    start(callback) {
+        let self = this;
+        let start = null;
+        this.running = true;
+        let remainingSeconds = this.els.seconds.textContent = this.duration / 1000;
+        this.els.definition.textContent = '';
+
+        function draw(now) {
+            if (!start) start = now;
+            let diff = now - start;
+            let newSeconds = Math.ceil((self.duration - diff)/1000);
+
+            if (diff <= self.duration) {
+                self.els.ticker.style.height = 100 - (diff/self.duration*100) + '%';
+
+                if (newSeconds != remainingSeconds) {
+                    self.els.seconds.textContent = newSeconds;
+                    remainingSeconds = newSeconds;
+                }
+
+                self.frameReq = window.requestAnimationFrame(draw);
+            } else {
+                self.running = false;
+                self.els.ticker.style.height = '0%';
+                self.els.seconds.textContent = 0;
+                self.changeWord();
+            }
+        };
+        self.frameReq = window.requestAnimationFrame(draw);
+    }
+
+    changeWord() {
+        // Reset CSS animation
+        this.element.classList.remove('countdown--ended');
+        void this.element.offsetWidth;
+        this.element.classList.add('countdown--ended');
+
+        // Update word until different from previous one
+        let newWord;
+        do {
+          newWord = this.words[this.index];
+          this.index++;
+          if (this.index===this.words.length) {
+            this.index=0;
+            this.current_level=this.current_level%3+1;
+            newList(this.current_level);
+          }
+        } while (newWord[2] === this.els.seconds.textContent);
+        this.word = newWord;
+        this.els.seconds.textContent = newWord[2];
+        if(this.helper) {
+            this.els.definition.textContent = this.word[4];
+        }
+        clearTimeout(this.timeout);
+        // timer for next word
+        this.timeout = setTimeout(() => { this.changeWord(); }, this.wordTimer);
+    }
+
+    toggleHelpers() {
+        if (this.helper) {
+            this.helper = false;
+            this.els.definition.textContent = '';
+        }
+        else {
+            this.helper = true;
+            this.els.definition.textContent = this.word[4];
+        }
+    }
+
+    toggleDefinition() {
+        if (this.definition) {
+            this.definition = false;
+            this.timeout = setTimeout(() => { this.changeWord(); }, this.wordTimer);
+            if (this.helper) {
+                this.els.definition.textContent = this.word[4];
+            }
+            else {
+                this.els.definition.textContent = '';
+            }
+        }
+        else {
+            this.definition = true;
+            clearInterval(this.timeout);
+            this.timeout = null;
+            this.els.definition.textContent = `"${this.word[3]}"`;
+        }
+    }
+
+    reset() {
+        this.running = false;
+        window.cancelAnimationFrame(this.frameReq);
+        clearTimeout(this.timeout);
+        this.els.seconds.textContent = this.duration / 1000;
+        this.els.ticker.style.height = null;
+        this.element.classList.remove('countdown--ended');
+    }
+
+    setDuration(duration) {
+        this.duration = duration;
+        this.els.seconds.textContent = this.duration / 1000;
+    }
+
+    setWords(words) {
+        this.words = words;
+    }
+    setTime(time) {
+      this.wordTimer = time;
+    }
+
+    //go to next word if the right arrow key is pressed
+    checkKey(e) {
+      e = e || window.event;
+      if (e.keyCode=='39') {
+        this.running = false;
+        this.els.ticker.style.height = '0%';
+        this.els.seconds.textContent = 0;
+        this.changeWord();
+      }
+    }
+}
